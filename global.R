5fd626669977ddc57765baf9d7baacef4380dace
Eliot McIntire <eliotmci@mail.ubc.ca>
2025-10-07 13:46:23 -0700

    submodules

--- global.R
+++ global.R
@@ -8,14 +8,20 @@ projectDir <- "~/GitHub/LandR_testing/"
 dir.create(projectDir, recursive = TRUE, showWarnings = FALSE)
 setwd(projectDir)
 
-# Require::Install("PredictiveEcology/SpaDES.project@development (>=0.1.1.9012)")
-# pkgload::load_all("~/GitHub/SpaDES.project/");
-# devtools::install("~/GitHub/SpaDES.project/");
-# devtools::install("~/GitHub/reproducible/");
-# pkgload::load_all("~/GitHub/SpaDES.project/");
 inSim <- SpaDES.project::setupProject(
-  modules = c("PredictiveEcology/Biomass_borealDataPrep@development",
-              "PredictiveEcology/Biomass_speciesData@development"
+  modules = c("PredictiveEcology/Biomass_borealDataPrep@development"# ,
+              # "PredictiveEcology/Biomass_speciesData@development"
   ),
-  useGit = "eliotmcintire"
+  packages = "googledrive",
+  useGit = "eliotmcintire",
+  options = options("spades.inputPaths" = "~/data"),
+  studyArea =
+    terra::as.polygons(terra::ext(c(xmin = 5706900, xmax = 5971300, ymin = 2047900, ymax = 2263800)),
+                       crs = "EPSG:3348")
 )
+if (FALSE) {
+  devtools::install("~/GitHub/reproducible/", upgrade = FALSE);
+  devtools::install("~/GitHub/SpaDES.core/", upgrade = FALSE);
+  devtools::install("~/GitHub/LandR/", upgrade = FALSE);
+}
+simOut <- SpaDES.core::simInitAndSpades2(inSim)
